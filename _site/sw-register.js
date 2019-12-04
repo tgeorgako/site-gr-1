2d729a03b31aa8a3cf866934028b873ec0e9ae7e
moya-decka <dramaquery@gmail.com>
2019-12-04 19:30:45 +0200

    message

--- _site/sw-register.js
+++ _site/sw-register.js
@@ -1,9 +1,9 @@
                         importScripts("/site-gr/workbox-v3.6.3/workbox-sw.js");
             workbox.setConfig({modulePathPrefix: "/site-gr/workbox-v3.6.3"});
 
-            self.__precacheManifest = [{"url":"/site-gr//index.html","revision":"64aba50d4468e31666c9e3a4f2d33e36"}];
+            self.__precacheManifest = [{"url":"/site-gr//index.html","revision":"696f300810d9230688f876cedf9bac80"}];
                     if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/site-gr//sw-register.js?v=1575235464').then(function(reg) {
+            navigator.serviceWorker.register('/site-gr//sw-register.js?v=1575480596').then(function(reg) {
                 reg.onupdatefound = function() {
                     var installingWorker = reg.installing;
                     installingWorker.onstatechange = function() {
